109b3f2cace320c89e048300df51ffc23cb6cacc
Leandro Franklin <leandrofranklin@msn.com>
2026-02-22 18:06:49 -0300

    Commit inicial

--- roles/relatorio-ha-vcenter/files/get_ha_events.py
+++ roles/relatorio-ha-vcenter/files/get_ha_events.py
@@ -28,56 +28,63 @@ def main():
         vcenter_time = si.CurrentTime()
         start_time = vcenter_time - timedelta(days=2)
 
-        # 1. Filtro de tempo
+        # Filtro pedindo APENAS pelo tempo (todas as categorias, todos os tipos)
         time_filter = vim.event.EventFilterSpec.ByTime()
         time_filter.beginTime = start_time
         time_filter.endTime = vcenter_time
 
-        # 2. Criando a especificação do filtro
         filter_spec = vim.event.EventFilterSpec()
         filter_spec.time = time_filter
-        filter_spec.eventTypeId = ["com.vmware.vc.ha.VmRestartedByHAEvent"]
-
-        # 3. NOVIDADE: Forçar a busca explícita a partir da raiz do vCenter
-        entity_filter = vim.event.EventFilterSpec.ByEntity()
-        entity_filter.entity = si.content.rootFolder
-        entity_filter.recursion = vim.event.EventFilterSpec.RecursionOption.all
-        filter_spec.entity = entity_filter
 
         event_manager = si.content.eventManager
-        
-        # 4. NOVIDADE: Usar o Collector em vez de QueryEvents
         collector = event_manager.CreateCollectorForEvents(filter_spec)
         
         events = []
         while True:
-            # Paginação: lê os eventos em blocos para não perder dados simultâneos
-            page = collector.ReadNextEvents(100)
+            # Baixa de 500 em 500 para não estourar o limite do SOAP da VMware
+            page = collector.ReadNextEvents(500)
             if not page:
                 break
             events.extend(page)
             
-        # Destruir o collector para limpar a memória no vCenter
         collector.DestroyCollector()
 
         ha_vms = []
         
+        # Filtro Fino Feito 100% no Python (Ignora os bugs de busca do vCenter)
         for event in events:
             msg = getattr(event, 'fullFormattedMessage', '')
             if not msg:
-                msg = "vSphere HA restarted this virtual machine"
+                msg = ''
                 
-            vm_name = "Desconhecida"
-            if getattr(event, 'vm', None) and event.vm is not None:
-                vm_name = event.vm.name
+            event_type_name = type(event).__name__
+            event_type_id = getattr(event, 'eventTypeId', '')
             
-            ha_vms.append({
-                "vm_name": vm_name,
-                "data_evento": event.createdTime.strftime("%Y-%m-%d %H:%M:%S UTC"),
-                "mensagem": msg,
-                "tipo_evento": "com.vmware.vc.ha.VmRestartedByHAEvent"
-            })
+            is_ha_restart = False
+            
+            # Checa todas as possibilidades de ser um evento de HA
+            if event_type_id == "com.vmware.vc.ha.VmRestartedByHAEvent":
+                is_ha_restart = True
+            elif "VmRestartedOnAlternateHostEvent" in event_type_name:
+                is_ha_restart = True
+            elif "VmDasBeingResetEvent" in event_type_name:
+                is_ha_restart = True
+            elif "vSphere HA restarted virtual machine" in msg:
+                is_ha_restart = True
+                
+            # Se for HA, garante que temos a VM afetada
+            if is_ha_restart:
+                if getattr(event, 'vm', None) and event.vm is not None:
+                    vm_name = event.vm.name
+                    
+                    ha_vms.append({
+                        "vm_name": vm_name,
+                        "data_evento": event.createdTime.strftime("%Y-%m-%d %H:%M:%S UTC"),
+                        "mensagem": msg,
+                        "tipo_evento": event_type_id if event_type_id else event_type_name
+                    })
 
+        # Retorna a lista final para o Ansible
         print(json.dumps(ha_vms))
 
     except Exception as e:
